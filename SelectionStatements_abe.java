25224a9c89ca1736f81ce1584c5f43d790059d0f
abeabemaria <ochiaabe@gmail.com>
2024-11-13 09:34:19 +0800

    Add files via upload

---
+++ SelectionStatements_abe.java
@@ -0,0 +1,26 @@
+
+package selection.statements._abe;
+
+
+public class SelectionStatements_abe {
+
+    public static void main(String[] args) {
+   
+        int num =3;
+        
+        if (num >= 0){
+            System.out.println("First String");
+          
+        }
+        if (num == 0){
+            System.out.println("Second String");
+            
+        }
+        else{
+           System.out.println("Third String");
+          
+        }
+        
+    }
+    
+}
